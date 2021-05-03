54176159ab82cae9c47d5fe897aae556f4664a0c
Maksym Honcharuk <gontcharuk@gmail.com>
2021-05-03 12:03:02 +0200

    Initial with linkedlist

---
+++ index.js
@@ -0,0 +1,109 @@
+class Node {
+    constructor (data, next = null) {
+        this.data = data
+        this.next = next
+    }
+}
+
+class LinkedList {
+    constructor () {
+        this.head = null
+        this.tail = null
+    }
+
+    append (data) {
+        const node = new Node(data)
+
+        if (this.tail) {
+            this.tail.next = node
+        }
+
+        if (!this.head) {
+            this.head = node
+        }
+
+        this.tail = node
+    }
+
+    prepend (data) {
+        const node = new Node(data, this.head)
+
+        this.head = node
+
+        if (!this.tail) {
+            this.tail = node
+        }
+    }
+
+    inseartAfter (after, data) {
+        const found = this.find(after)
+
+        if (!found) {
+            return
+        }
+
+        found.next = new Node(data, found.next)
+    }
+
+    toArray () {
+        let current = this.head
+        const output = []
+
+        while (current) {
+            output.push(current)
+            current = current.next
+        }
+
+        return output
+    }
+
+    find (data) {
+        if (!this.head) {
+            return 
+        }
+
+        let current = this.head
+        while (current) {
+            if (current.data === data) {
+                return current
+            }
+            current = current.next
+        } 
+    }
+
+    remove (data) {
+        if (!this.head) {
+            return
+        }
+
+        while (this.head && this.head.data === data) {
+            this.head = this.head.next
+        }
+
+        let current = this.head
+        while (current.next) {
+            if (current.next.data === data) {
+                current.next = current.next.next
+            } else {
+                current = current.next
+            }
+        }
+
+        if (this.tail.data === data) {
+            this.tail = current
+        }
+    }
+}
+
+const list = new LinkedList
+list.prepend('Hi')
+list.append('My')
+list.append('Name')
+list.append('Maks')
+list.inseartAfter('Name', 'is')
+list.prepend(24)
+list.append(45)
+list.remove(45)
+list.remove(24)
+
+console.log(list.toArray())
